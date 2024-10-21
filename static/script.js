235778154631b00e81b05444b5e43cfbbedce6bc
EvanBClark <purplewalruscontact@gmail.com>
2024-10-21 13:39:02 -0700

    chrome bug

--- static/script.js
+++ static/script.js
@@ -43,16 +43,27 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // If window is resized, redraw game
+let resizeTimeout;
 let lastViewportHeight = window.visualViewport.height;
+let keyboardOpen = false;
+
 window.addEventListener('resize', function() {
     const currentViewportHeight = window.visualViewport.height;
-    // Check if the viewport height has decreased significantly
+
     if (currentViewportHeight < lastViewportHeight * 0.9) {
-        console.log('Soft keyboard is likely open');
+        if (!keyboardOpen) {
+            console.log('Soft keyboard is likely open');
+            keyboardOpen = true;
+        }
     } else {
-        console.log('Window resized or keyboard closed');
-        drawGame();
+        clearTimeout(resizeTimeout);
+        resizeTimeout = setTimeout(() => {
+            console.log('Window resized or keyboard closed');
+            drawGame();
+            keyboardOpen = false;
+        }, 300);
     }
+
     lastViewportHeight = currentViewportHeight;
 });
 
